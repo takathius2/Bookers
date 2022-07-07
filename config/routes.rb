186db74f037e2b33c3d6e306e5725d091e2e40df
takahius2 <106050338+takathius2@users.noreply.github.com>
2022-07-07 16:13:11 +0000

    [Add]とりあえず完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'books#top'
-  resources :books 
-  get 'books/:id/edit' => 'books#edit'
+  post 'books' => 'books#create'
+  resources :books
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
